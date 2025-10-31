07bfe281837767d31828665b17d265459bf98c02
Morris Ouedraogo <morrisoue19@icloud.com>
2025-10-31 18:02:18 +0000

    added the missing columns for the model

--- db/migrate/20251031104958_create_certificates.rb
+++ db/migrate/20251031104958_create_certificates.rb
@@ -3,7 +3,9 @@ class CreateCertificates < ActiveRecord::Migration[8.0]
     create_table :certificates do |t|
       t.references :employee, null: false, foreign_key: true
       t.references :course, null: false, foreign_key: true
+      t.string :name
       t.date :issued_on
+      t.date :expiry_date
       t.string :issuer
       t.text :notes
 
