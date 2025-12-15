76f43ab4af7c2ccc94427bc9157d7de33bef22a0
Morris Ouedraogo <morrisoue19@icloud.com>
2025-12-15 21:01:38 +0000

    for deploying on render

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -23,6 +23,12 @@ Rails.application.configure do
     config.action_controller.perform_caching = false
   end
 
+  Rails.application.routes.default_url_options = {
+    host: "localhost",
+    port: 3000,
+    protocol: "http"
+  }
+
   # Change to :null_store to avoid any caching.
   config.cache_store = :memory_store
 
