2b5138134090d6a797a929f0c2a3d8bce9691ac6
Morris Ouedraogo <morrisoue19@icloud.com>
2025-12-15 03:25:57 +0000

    Application base controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::API #Base controller for the API
+  before_action :set_current_employee_from_param #Ensures current_employee is available for controllers 
+
+  attr_reader :current_employee # Allows other controllers to access the employee without changing it 
+
+  private
+  # Retrieves the employee record using employee_id
+  def set_current_employee_from_param
+    @current_employee = Employee.find_by(id: params[:employee_id])
+  end 
+
+  # Global admin check for all controllers
+  def require_admin
+    unless current_employee&.admin?
+      render json: { error: "Admins only" }, status: :forbidden
+    end
+  end
 end
